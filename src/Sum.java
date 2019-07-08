5dc01efd17053fb5436922b88e4636de7d283f5b
Govardhan <govardhanyeruva5@gmail.com>
2019-07-08 19:53:56 +0530

    American change

--- src/Sum.java
+++ src/Sum.java
@@ -4,7 +4,11 @@ public class Sum {
 	public int summer(int a,int b)
 	{
 		int sum= a+b;
-		
+		System.out.println("Sample testing sum");
+
+		System.out.println("Sample testing sum 2");
+
+		System.out.println("Sample testing sum3");
 		return sum;
 				
 	}
